339f0d3a695a23df1e7ac270985ca1a8dee69153
zzes <kuanglong0312@sina.com>
2017-03-30 10:18:40 +0800

    change

--- Nodejs_Server/tcpiotclient.js
+++ Nodejs_Server/tcpiotclient.js
@@ -2,7 +2,7 @@ const net = require('net')
 const client = net.connect({port:3000},() => {
 	console.log("connected to server!");
 	while(1){
-		client.write("{\"tem\":\"20\",\"hum\":\"80\"}");
+		client.write("{\"tem\":\"20\",\"hum\":\"80\",\"indoor\":\"1\"}");
 	}
 	
 });
