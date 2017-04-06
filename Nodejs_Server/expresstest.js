33e255b9c5fe4659027e77836ab1f4b040a47744
zzes <kuanglong0312@sina.com>
2017-04-06 08:30:39 +0800

    接口

--- Nodejs_Server/expresstest.js
+++ Nodejs_Server/expresstest.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 app = express();
-
+app.use('/static',express.static('public'));
 //数据库配置
 var conn = mysql.createConnection({
 	host:'localhost',
