494e375b8888c2eca88ba86d3a1ecb95c011bbf5
zzes <kuanglong0312@sina.com>
2017-05-21 10:16:11 +0800

    tcp测试客户端

--- Nodejs_Server/tcpiotclient.js
+++ Nodejs_Server/tcpiotclient.js
@@ -1,9 +1,9 @@
 const net = require('net')
-const client = net.connect({port:3000},() => {
+const client = net.connect({port:9000},() => {
 	console.log("connected to server!");
-	while(1){
+	 
 		client.write("{\"tem\":\"20\",\"hum\":\"80\",\"indoor\":\"1\"}");
-	}
+ 
 	
 });
 client.on('data',(data) => {
